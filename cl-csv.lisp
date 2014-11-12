271c7acbb5234591b10d1f38c84e4ff1da5304e1
Riley <eltrich.riley@gmail.com>
2014-11-12 11:38:27 -0800

    removing garbage

--- cl-csv.lisp
+++
@@ -1,7 +0,0 @@
-
-(defun count-spaces (str)
-  (let ((space-count 0))
-    (iterate (for chr in-string str)
-      (when (char= chr #\SPACE)
-        (incf space-count))
-      (finally (return space-count)))))
